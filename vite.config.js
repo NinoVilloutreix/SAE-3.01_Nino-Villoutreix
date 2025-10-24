c5d09ec821bba032223cb18176d49b9ab1efd4be
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2025-10-24 16:08:06 +0200

    github page - test2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/SAE-301_Nino-Villoutreix",
+  build: {
+   rollupOptions: {
+     input: {
+       main: resolve(__dirname, "index.html"),
+       accueil: resolve(__dirname, "pages/accueil.html"),
+     },
+   },
+ },
 });
